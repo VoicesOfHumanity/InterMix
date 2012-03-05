4a262864645cf747139a8e8712cfd3ce9f164ea4
Flemming Funch <ffunch@cr8.com>
2012-03-05 02:40:31 +0100

    set current period

---
+++ script/set_current_period.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+# Set the current period for discussions, based on dates
+
+require File.dirname(__FILE__)+'/cron_helper'
+
+today = Time.now.strftime("%Y-%m-%d")
+
+dialogs = Dialog.order("id")
+
+for dialog in dialogs
+  print "#{dialog.id}: #{dialog.name}\n"
+  
+  periods = Period.where("dialog_id=#{dialog.id}").order("id")
+  
+  for period in periods
+    if period.startdate.to_s != '' and period.endposting.to_s != '' and period.endrating.to_s != ''
+      print " - #{period.startdate} - #{period.endposting}/#{period.endrating}\n"
+      if  today >= period.startdate and (today <= period.endposting or today <= period.endrating)
+        if dialog.current_period == period.id
+          dialog.current_period = period.id
+          dialog.save!
+          print " - - set to CURRENT\n"          
+        else  
+          print " - - was already current\n"          
+        end
+      end
+    end
+  end
+
+end
