dcae88eea99d75eafe72de495f00e4b8b3f7e5dc
Flemming Funch <ffunch@cr8.com>
2012-03-18 23:10:31 +0100

    postmark delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,8 @@ Intermix::Application.configure do
   ### ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'intermix.dev' }
   if true
-    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.delivery_method   = :postmark
+    #config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       :address => "mail.worldtrans.org",
       :port => 25,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,8 @@ Intermix::Application.configure do
   config.action_mailer.default_url_options = { :host => BASEDOMAIN }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.delivery_method = :sendmail
+  #config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method   = :postmark
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
