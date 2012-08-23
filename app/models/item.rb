0cb8b1fbb21371e1235001943948d0c2c02f4de2
Flemming Funch <ffunch@cr8.com>
2012-08-23 19:59:44 +0200

    mailing fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -115,7 +115,9 @@ class Item < ActiveRecord::Base
       #elsif group and not (recipient.group_email=='instant' or recipient.forum_email=='instant')
       #  logger.info("#{recipient.id}:#{recipient.name} is not set for instant group mail, so skipping")
       #  next
-      elsif not group and not recipient.forum_email=='instant'
+      elsif not group
+        next
+      elsif not recipient.forum_email=='instant'
         logger.info("#{recipient.id}:#{recipient.name} is not set for instant forum mail, so skipping")
         next
       end  
