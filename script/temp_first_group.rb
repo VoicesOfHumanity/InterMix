07de9aa029041648cb18b841b3f868ba53d607eb
Flemming Funch <ffunch@cr8.com>
2012-09-29 00:11:22 +0200

    fix

--- script/temp_first_group.rb
+++ script/temp_first_group.rb
@@ -11,7 +11,7 @@ for item in items
     item.first_in_thread_group_id = item.group_id
     item.save
   elsif item.first_in_thread.to_i > 0
-    first_item = Item.find(item.first_in_thread)
+    first_item = Item.find_by_id(item.first_in_thread)
     item.first_in_thread_group_id = first_item.group_id if first_item
     item.save
   end
