254cd25ec7d16d591b50db60694b988c2a5254a7
Flemming Funch <ffunch@cr8.com>
2012-11-13 23:41:51 +0100

    fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,11 @@
 class RegistrationsController < Devise::RegistrationsController
   layout "front"
 
+  def new
+    logger.debug("Registrations#new")
+    super
+  end
+
   def create
     logger.debug("Registrations#create")
     
@@ -18,6 +23,7 @@ class RegistrationsController < Devise::RegistrationsController
   
   def build_resource(*args)
     logger.debug("Registrations#build_resource")
+    @metamaps = Metamap.where(:global_default=>true)
     super
     if session[:omniauth]
       @participant.apply_omniauth(session[:omniauth])
