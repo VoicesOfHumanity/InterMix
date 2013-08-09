5b2b0f2c7d59f701deb56583422b3cf8e2272457
Flemming Funch <ffunch@cr8.com>
2013-08-09 02:20:22 +0200

    fix

--- app/mailers/receive_mailer.rb
+++ app/mailers/receive_mailer.rb
@@ -160,7 +160,7 @@ end
     end 
     
     #-- Try to remove old quotes, footers, personal login info, etc.
-    html_content.gsub!(%r{[0-9]+/[0-9]+/[0-9]+ InterMix <questions@intermix.org>.*$}m,"")
+    html_content.gsub!(%r{[0-9]+/[0-9]+/[0-9]+ InterMix .*$}m,"")
     html_content.gsub!(/<p id="following">.*$/m,"")
     html_content.gsub!(/<p id="footer">.*$/m,"")
     html_content.gsub!(/auth_token=[^"]+/,"")
