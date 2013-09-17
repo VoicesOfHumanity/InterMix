bccbae28d5e0561c45a04c091d03c520976f7395
Flemming Funch <ffunch@cr8.com>
2013-09-17 02:46:48 +0200

    fix

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,7 @@ class ItemsController < ApplicationController
   def list
     #-- Return the items to show on the page
     @from = params[:from] || ''
-    @ratings = params[:ratings]
+    @ratings = (params[:ratings].to_i == 1)
     @sortby = params[:sortby] || "items.id desc"
     @perscr = params[:perscr].to_i || 25
     @threads = params[:threads] || 'flat'
