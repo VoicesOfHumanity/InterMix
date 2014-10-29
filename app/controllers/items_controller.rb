83696bb2bdb0ea642046b47ecffb5d95b193ff1e
ffunch <ffunch@cr8.com>
2014-10-29 19:41:14 +0100

    fix

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -206,7 +206,7 @@ class ItemsController < ApplicationController
     
     @replies = Item.where("is_first_in_thread=0 and first_in_thread=#{item.id}").order("id").all
     
-    @itemsproc   NB !!!!!!!!!!!!!!!!!!!! Where does that come from?
+    #@itemsproc   NB !!!!!!!!!!!!!!!!!!!! Where does that come from?
     
     render :partial=>'item_dsimple_comments', :locals => { :item => item, :replies => @replies, :odd_or_even => 1, :from => 'dsimple'}, :layout=>false       
   end  
