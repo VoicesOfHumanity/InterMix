ce2b1772ef231a5a515b6865a5a1aa36aa9aae23
ffunch <ffunch@cr8.com>
2015-01-06 18:14:22 +0100

    slider fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -488,7 +488,8 @@ class Item < ActiveRecord::Base
     items = items.where("items.created_at<='#{end_at.strftime("%Y-%m-%d %H:%M:%S")}'") if end_at.to_s != ''
 
     #-- We'll add up the stats, but we're including the overall rating summary anyway
-    items = items.includes([:dialog,:group,:period,{:participant=>{:metamap_node_participants=>:metamap_node}},:item_rating_summary])
+    items = items.joins(:participant=>{:metamap_node_participants=>:metamap_node})
+    items = items.includes([:dialog,:group,:period,:item_rating_summary])
     
     if participant_id.to_i > 0
       #-- If a participant_id is given, we'll include that person's rating for each item, if there is any
