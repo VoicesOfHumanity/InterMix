c948cb72dcd03b7ae07277f0d70dac053321b4b4
ffunch <ffunch@cr8.com>
2015-04-01 18:29:55 +0200

    authenticating optout

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -10,6 +10,7 @@ class FrontController < ApplicationController
 	
 	before_filter :check_status
   
+  append_before_action :authenticate_user_from_token!, :only=>[:optout,:optout_confirm]
   append_before_action :authenticate_participant!, :only=>[:optout,:optout_confirm]
   
   def index
