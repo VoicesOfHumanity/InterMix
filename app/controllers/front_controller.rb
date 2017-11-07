84434c0170466455dac0e77de8a17a64f1de9ea7
Flemming Fuunch <ffunch@cr8.com>
2017-11-07 23:43:10 +0100

    join with community

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -261,6 +261,7 @@ class FrontController < ApplicationController
   
   def dialogjoinform
     #-- Form for submitting to join a dialog
+    @comtag = params[:comtag]
     @group_id,@dialog_id = get_group_dialog_from_subdomain    
     @dialog_id = params[:dialog_id].to_i if not @dialog_id
     @dialog_id = params[:id].to_i if not @dialog_id or @dialog_id == 0
