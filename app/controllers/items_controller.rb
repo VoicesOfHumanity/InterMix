5ebbcf310264141069beaa4da566d83d4765f22b
ffunch <ffunch@cr8.com>
2018-10-09 17:32:01 +0200

    fixing thumb problem

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1012,6 +1012,7 @@ class ItemsController < ApplicationController
     item_id = item.id
     group_id = item.group_id.to_i
     dialog_id = item.dialog_id.to_i
+    is_new = false
     
     # check for comments in that thread
     #com_count = Item.where(posted_by: current_participant.id, is_first_in_thread: false, first_in_thread: item.first_in_thread).count
@@ -1019,9 +1020,15 @@ class ItemsController < ApplicationController
     com_count = Item.where(posted_by: current_participant.id, reply_to: item_id).count
     
     #-- See if that user already has rated that item, or create a new rating if they haven't
-    rating = Rating.where(item_id: item_id, participant_id: current_participant.id, rating_type: 'AllRatings').first_or_initialize
+    rating = Rating.where(item_id: item_id, participant_id: current_participant.id, rating_type: 'AllRatings').first
+    if not rating
+      is_new = true
+      rating = Rating.create(item_id: item_id, participant_id: current_participant.id, rating_type: 'AllRatings', approval: vote, interest: vote.abs)
+    end
     
-    if rating and from_mail
+    if is_new
+      # We just saved the initial rating
+    elsif rating and from_mail
       #-- If it was from an email, there's only -1 and +1 choice. If we already had a rating, only do something if it changed direction
       # https://intermix.test:3002/items/2111/view?auth_token=Y8fCCBYWx8-ETHcyWGC4&thumb=-1
       if rating.approval and rating.approval.to_i > 0 and vote < 0
@@ -1041,6 +1048,7 @@ class ItemsController < ApplicationController
       rating.approval = vote    
       rating.interest = vote.abs
     end
+    
     if com_count > 0
       rating.interest = 4
     end
