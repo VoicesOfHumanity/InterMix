64f105e4ce33531c9e3827d3f2a9213e539fb260
ffunch <ffunch@cr8.com>
2018-11-24 00:43:58 +0100

    precompile ckeditor/config.js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0.1'
+Rails.application.config.assets.version = '1.0.2'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -11,4 +11,4 @@ Rails.application.config.assets.version = '1.0.1'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 # not included everywhere, but specifically linked to:
-Rails.application.config.assets.precompile += %w( admin.css formtastic.css formtastic_changes.css pepper-grinder/jquery-ui-1.8.6.custom.css blueprint/print.css blueprint/ie.css ckeditor/* )
+Rails.application.config.assets.precompile += %w( admin.css formtastic.css formtastic_changes.css pepper-grinder/jquery-ui-1.8.6.custom.css blueprint/print.css blueprint/ie.css ckeditor/config.js )
