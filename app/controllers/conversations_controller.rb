38c0da69c78be5ddb0f101b3542e78d0ba378c89
ffunch <ffunch@cr8.com>
2019-10-29 21:09:44 +0100

    city fix

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -190,7 +190,7 @@ class ConversationsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def conversation_params
-      params.require(:conversation).permit(:name, :shortname, :description, :front_template)
+      params.require(:conversation).permit(:name, :shortname, :description, :front_template, :together_apart)
     end
     
     def check_is_admin
