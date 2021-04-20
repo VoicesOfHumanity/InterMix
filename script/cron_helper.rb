56ccca9c606679294b2210141bc02e136713cc16
ffunch <ffunch@cr8.com>
2021-04-20 23:02:08 +0200

    missing profile info

--- script/cron_helper.rb
+++ script/cron_helper.rb
@@ -7,3 +7,15 @@ def helper
 end
 helper.extend ApplicationHelper
 helper.extend ActionView::Helpers::TextHelper
+
+# This is to make logger redirect to the console
+logger                                         = Logger.new(STDOUT)
+logger.level                                   = Logger::DEBUG
+RAILS_DEFAULT_LOGGER                           = logger
+ActiveRecord::Base.logger                      = logger
+ActionController::Base.logger                  = logger
+#ActiveSupport::Cache::MemCacheStore.logger     = logger
+ApplicationController.allow_forgery_protection = false
+#reload!
+
+#logger.info("cron_helper")
