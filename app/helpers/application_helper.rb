a6fdf852e0eee13d7c20ccb85709ee7e0459dff6
ffunch <ffunch@cr8.com>
2021-04-27 23:02:43 +0200

    urls in question explanation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -180,5 +180,8 @@ module ApplicationHelper
     end
   end        
         
-      
+  def active_links(text)
+    # Turn any plan urls into active html links in a text. Fx for question type items
+    simple_format(text).gsub(URI.regexp, '<a href="\0" target="_blank">\0</a>').html_safe
+  end 
 end
