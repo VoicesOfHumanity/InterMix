5ab0579f53361552746866792530bcee1e8b326b
ffunch <ffunch@cr8.com>
2023-01-23 00:17:20 +0100

    Revert "started setting up ckeditor_rails_6, but will go back to rails 5 for now"
    This reverts commit 200e365dcfcb87490f1ce67d792e3a852a3d8417.

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,16 +1,3 @@
-Ckeditor::Rails.configure do |config|
-  # default is nil for all languages, or set as %w[en zh]
-  config.assets_languages = nil
-
-  # default is nil for all plugins,
-  # or set as white list: %w[image link liststyle table tabletools]
-  # or set as black list: config.default_plugins - %w[about a11yhelp]
-  config.assets_plugins = nil
-
-  # default is nil for all skins, or set as %w[moono-lisa]
-  config.assets_skins = nil
-end
-
 # Use this hook to configure ckeditor. Probably no longer needed
 #Ckeditor.setup do |config|
   # ==> ORM configuration
