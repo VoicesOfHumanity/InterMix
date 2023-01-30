9a9d6b353c37719e980c72542f7977e63a0ecd47
ffunch <ffunch@cr8.com>
2023-01-30 02:17:40 +0100

    bug in api update

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -101,7 +101,7 @@ class ApiController < ApplicationController
                 p.city_uniq = data['admin1uniq'] + '_' + data['city']
             end
             if data.has_key?('country_code2')
-                p.country_code = data['country_code2']
+                p.country_code2 = data['country_code2']
             end
             if data.has_key?('generation_id')
                 p.update_generation(data['generation_id'])
