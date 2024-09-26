47a1cc7646141aeb13b94d1248afbd5174f87d02
ffunch <ffunch@cr8.com>
2024-09-26 23:38:23 +0200

    removing some secrets

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Intermix::Application.config.secret_token = '48093fed6133a31b84a512faa083d5b9da7609ec817d74ae22bd62bed20dbba16940a3b587fed07876ea4e80679ed286dd6c3fb3589380744f53a1b55151e01c'
